8ba1d0ab0826d755c5cfbb9d63548c877e195ac3
MariusArhaug <mariusarhaug@hotmail.com>
2020-11-06 17:58:52 +0100

    algorithm now includes negative numbers in list

--- highestProduct.py
+++ highestProduct.py
@@ -1,12 +1,30 @@
-def highestProduct(listA,n):
-    listA.sort(reverse=True) #sort in descending order, the three first elements are the biggest
-    listA = listA[:n]
+def highestProduct(listA): 
+    length = len(listA)
+    if length < 3:
+        return "List is not big enough to find biggest product of 3 integers!" 
+    listA.sort() #ascending order
 
-    product = 1
+    #Check if a list only contains negative numbers
+    count = 0
     for integer in listA:
-        product *= integer
-    return product
+        if integer < 0:
+            count += 1
+    if (count == length):
+        return "Cannot find biggest triplet in a list with only"
+ 
+    #find triplet with two biggest negative numbers = positive * biggest positive number
+    biggestNegative = listA[0] * listA[1] * listA[length-1]
 
-listA = [1,10,2,6,5,3]
-print(highestProduct(listA,3))
+    #find triplet with three biggest positive numbers
+    biggestPositive = listA[length-1] * listA[length-2] * listA[length-3]
+   
+    finalProduct = max(biggestNegative, biggestPositive)
+    return finalProduct
+
+listA = [1,10,2,5,6,3]
+print(highestProduct(listA))
+
+listB = [50,20,1,-25,-25,-25]
+print(highestProduct(listB))
+#print(highestProduct(listB,3))
 
